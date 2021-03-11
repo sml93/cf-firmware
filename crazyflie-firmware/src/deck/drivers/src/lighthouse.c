1bf5c68bc0cf3c86938c443cf8623c0f278b16e0
sml93 <shawnmlee93@gmail.com>
2021-03-11 20:24:40 +0800

    cf-firmware for AFC CDE3

---
+++ crazyflie-firmware/src/deck/drivers/src/lighthouse.c
@@ -0,0 +1,91 @@
+/**
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2018-2020 Bitcraze AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * lighthouse.c: lighthouse tracking system receiver
+ */
+
+#include "deck.h"
+#include "param.h"
+
+#include "stm32fxxx.h"
+#include "config.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+
+#include "lighthouse_core.h"
+
+// LED timer
+static StaticTimer_t timerBuffer;
+#define FIFTH_SECOND 200
+static void ledTimerHandle(xTimerHandle timer);
+
+static bool isInit = false;
+// lighthouseBaseStationsGeometry has been moved to lighthouse_core.c
+
+static void lighthouseInit(DeckInfo *info)
+{
+  if (isInit) {
+    return;
+  }
+
+  lighthouseCoreInit();
+
+  xTaskCreate(lighthouseCoreTask, LIGHTHOUSE_TASK_NAME,
+              2*configMINIMAL_STACK_SIZE, NULL, LIGHTHOUSE_TASK_PRI, NULL);
+
+  xTimerHandle timer;
+  timer = xTimerCreateStatic("ledTimer", M2T(FIFTH_SECOND), pdTRUE,
+    NULL, ledTimerHandle, &timerBuffer);
+  xTimerStart(timer, M2T(0));
+
+  isInit = true;
+}
+
+static void ledTimerHandle(xTimerHandle timer) {
+  lighthouseCoreLedTimer();
+}
+
+static const DeckDriver lighthouse_deck = {
+  .vid = 0xBC,
+  .pid = 0x10,
+  .name = "bcLighthouse4",
+
+  .usedGpio = 0,  // FIXME: set the used pins
+  .requiredEstimator = kalmanEstimator,
+
+  .init = lighthouseInit,
+};
+
+
+DECK_DRIVER(lighthouse_deck);
+
+PARAM_GROUP_START(deck)
+PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, bcLighthouse4, &isInit)
+PARAM_GROUP_STOP(deck)
---
+++ crazyflie-firmware/src/modules/src/stabilizer.c
@@ -0,0 +1,426 @@
+/**
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie Firmware
+ *
+ * Copyright (C) 2011-2016 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+#define DEBUG_MODULE "STAB"
+
+#include <math.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "system.h"
+#include "log.h"
+#include "param.h"
+#include "debug.h"
+
+#include "stabilizer.h"
+
+#include "sensors.h"
+#include "commander.h"
+#include "crtp_localization_service.h"
+#include "sitaw.h"
+#include "controller.h"
+#include "power_distribution.h"
+#include "health.h"
+
+#include "estimator.h"
+#include "usddeck.h"
+#include "quatcompress.h"
+#include "statsCnt.h"
+#include "static_mem.h"
+
+static bool isInit;
+static bool emergencyStop = false;
+static int emergencyStopTimeout = EMERGENCY_STOP_TIMEOUT_DISABLED;
+
+uint32_t inToOutLatency;
+
+// State variables for the stabilizer
+static setpoint_t setpoint;
+static sensorData_t sensorData;
+static state_t state;
+static control_t control;
+
+static StateEstimatorType estimatorType;
+static ControllerType controllerType;
+
+static STATS_CNT_RATE_DEFINE(stabilizerRate, 500);
+
+static struct {
+  // position - mm
+  int16_t x;
+  int16_t y;
+  int16_t z;
+  // velocity - mm / sec
+  int16_t vx;
+  int16_t vy;
+  int16_t vz;
+  // acceleration - mm / sec^2
+  int16_t ax;
+  int16_t ay;
+  int16_t az;
+  // compressed quaternion, see quatcompress.h
+  int32_t quat;
+  // angular velocity - milliradians / sec
+  int16_t rateRoll;
+  int16_t ratePitch;
+  int16_t rateYaw;
+} stateCompressed;
+
+static struct {
+  // position - mm
+  int16_t x;
+  int16_t y;
+  int16_t z;
+  // velocity - mm / sec
+  int16_t vx;
+  int16_t vy;
+  int16_t vz;
+  // acceleration - mm / sec^2
+  int16_t ax;
+  int16_t ay;
+  int16_t az;
+} setpointCompressed;
+
+STATIC_MEM_TASK_ALLOC(stabilizerTask, STABILIZER_TASK_STACKSIZE);
+
+static void stabilizerTask(void* param);
+
+static void calcSensorToOutputLatency(const sensorData_t *sensorData)
+{
+  uint64_t outTimestamp = usecTimestamp();
+  inToOutLatency = outTimestamp - sensorData->interruptTimestamp;
+}
+
+static void compressState()
+{
+  stateCompressed.x = state.position.x * 1000.0f;
+  stateCompressed.y = state.position.y * 1000.0f;
+  stateCompressed.z = state.position.z * 1000.0f;
+
+  stateCompressed.vx = state.velocity.x * 1000.0f;
+  stateCompressed.vy = state.velocity.y * 1000.0f;
+  stateCompressed.vz = state.velocity.z * 1000.0f;
+
+  stateCompressed.ax = state.acc.x * 9.81f * 1000.0f;
+  stateCompressed.ay = state.acc.y * 9.81f * 1000.0f;
+  stateCompressed.az = (state.acc.z + 1) * 9.81f * 1000.0f;
+
+  float const q[4] = {
+    state.attitudeQuaternion.x,
+    state.attitudeQuaternion.y,
+    state.attitudeQuaternion.z,
+    state.attitudeQuaternion.w};
+  stateCompressed.quat = quatcompress(q);
+
+  float const deg2millirad = ((float)M_PI * 1000.0f) / 180.0f;
+  stateCompressed.rateRoll = sensorData.gyro.x * deg2millirad;
+  stateCompressed.ratePitch = -sensorData.gyro.y * deg2millirad;
+  stateCompressed.rateYaw = sensorData.gyro.z * deg2millirad;
+}
+
+static void compressSetpoint()
+{
+  setpointCompressed.x = setpoint.position.x * 1000.0f;
+  setpointCompressed.y = setpoint.position.y * 1000.0f;
+  setpointCompressed.z = setpoint.position.z * 1000.0f;
+
+  setpointCompressed.vx = setpoint.velocity.x * 1000.0f;
+  setpointCompressed.vy = setpoint.velocity.y * 1000.0f;
+  setpointCompressed.vz = setpoint.velocity.z * 1000.0f;
+
+  setpointCompressed.ax = setpoint.acceleration.x * 1000.0f;
+  setpointCompressed.ay = setpoint.acceleration.y * 1000.0f;
+  setpointCompressed.az = setpoint.acceleration.z * 1000.0f;
+}
+
+void stabilizerInit(StateEstimatorType estimator)
+{
+  if(isInit)
+    return;
+
+  sensorsInit();
+  stateEstimatorInit(estimator);
+  controllerInit(ControllerTypeAny);
+  powerDistributionInit();
+  sitAwInit();
+  estimatorType = getStateEstimator();
+  controllerType = getControllerType();
+
+  STATIC_MEM_TASK_CREATE(stabilizerTask, stabilizerTask, STABILIZER_TASK_NAME, NULL, STABILIZER_TASK_PRI);
+
+  isInit = true;
+}
+
+bool stabilizerTest(void)
+{
+  bool pass = true;
+
+  pass &= sensorsTest();
+  pass &= stateEstimatorTest();
+  pass &= controllerTest();
+  pass &= powerDistributionTest();
+
+  return pass;
+}
+
+static void checkEmergencyStopTimeout()
+{
+  if (emergencyStopTimeout >= 0) {
+    emergencyStopTimeout -= 1;
+
+    if (emergencyStopTimeout == 0) {
+      emergencyStop = true;
+    }
+  }
+}
+
+/* The stabilizer loop runs at 1kHz (stock) or 500Hz (kalman). It is the
+ * responsibility of the different functions to run slower by skipping call
+ * (ie. returning without modifying the output structure).
+ */
+
+static void stabilizerTask(void* param)
+{
+  uint32_t tick;
+  uint32_t lastWakeTime;
+  vTaskSetApplicationTaskTag(0, (void*)TASK_STABILIZER_ID_NBR);
+
+  //Wait for the system to be fully started to start stabilization loop
+  systemWaitStart();
+
+  DEBUG_PRINT("Wait for sensor calibration...\n");
+
+  // Wait for sensors to be calibrated
+  lastWakeTime = xTaskGetTickCount ();
+  while(!sensorsAreCalibrated()) {
+    vTaskDelayUntil(&lastWakeTime, F2T(RATE_MAIN_LOOP));
+  }
+  // Initialize tick to something else then 0
+  tick = 1;
+
+  DEBUG_PRINT("Ready to fly.\n");
+
+  while(1) {
+    // The sensor should unlock at 1kHz
+    sensorsWaitDataReady();
+
+    if (healthShallWeRunTest())
+    {
+      sensorsAcquire(&sensorData, tick);
+      healthRunTests(&sensorData);
+    } else {
+      // allow to update estimator dynamically
+      if (getStateEstimator() != estimatorType) {
+        stateEstimatorSwitchTo(estimatorType);
+        estimatorType = getStateEstimator();
+      }
+      // allow to update controller dynamically
+      if (getControllerType() != controllerType) {
+        controllerInit(controllerType);
+        controllerType = getControllerType();
+      }
+
+      stateEstimator(&state, &sensorData, &control, tick);
+      compressState();
+
+      commanderGetSetpoint(&setpoint, &state);
+      compressSetpoint();
+
+      sitAwUpdateSetpoint(&setpoint, &sensorData, &state);
+
+      controller(&control, &setpoint, &sensorData, &state, tick);
+
+      checkEmergencyStopTimeout();
+
+      if (emergencyStop) {
+        powerStop();
+      } else {
+        powerDistribution(&control);
+      }
+
+      // Log data to uSD card if configured
+      if (   usddeckLoggingEnabled()
+          && usddeckLoggingMode() == usddeckLoggingMode_SynchronousStabilizer
+          && RATE_DO_EXECUTE(usddeckFrequency(), tick)) {
+        usddeckTriggerLogging();
+      }
+    }
+    calcSensorToOutputLatency(&sensorData);
+    tick++;
+    STATS_CNT_RATE_EVENT(&stabilizerRate);
+  }
+}
+
+void stabilizerSetEmergencyStop()
+{
+  emergencyStop = true;
+}
+
+void stabilizerResetEmergencyStop()
+{
+  emergencyStop = false;
+}
+
+void stabilizerSetEmergencyStopTimeout(int timeout)
+{
+  emergencyStop = false;
+  emergencyStopTimeout = timeout;
+}
+
+PARAM_GROUP_START(stabilizer)
+PARAM_ADD(PARAM_UINT8, estimator, &estimatorType)
+PARAM_ADD(PARAM_UINT8, controller, &controllerType)
+PARAM_ADD(PARAM_UINT8, stop, &emergencyStop)
+PARAM_GROUP_STOP(stabilizer)
+
+LOG_GROUP_START(ctrltarget)
+LOG_ADD(LOG_FLOAT, x, &setpoint.position.x)
+LOG_ADD(LOG_FLOAT, y, &setpoint.position.y)
+LOG_ADD(LOG_FLOAT, z, &setpoint.position.z)
+
+LOG_ADD(LOG_FLOAT, vx, &setpoint.velocity.x)
+LOG_ADD(LOG_FLOAT, vy, &setpoint.velocity.y)
+LOG_ADD(LOG_FLOAT, vz, &setpoint.velocity.z)
+
+LOG_ADD(LOG_FLOAT, ax, &setpoint.acceleration.x)
+LOG_ADD(LOG_FLOAT, ay, &setpoint.acceleration.y)
+LOG_ADD(LOG_FLOAT, az, &setpoint.acceleration.z)
+
+LOG_ADD(LOG_FLOAT, roll, &setpoint.attitude.roll)
+LOG_ADD(LOG_FLOAT, pitch, &setpoint.attitude.pitch)
+LOG_ADD(LOG_FLOAT, yaw, &setpoint.attitudeRate.yaw)
+LOG_GROUP_STOP(ctrltarget)
+
+LOG_GROUP_START(ctrltargetZ)
+LOG_ADD(LOG_INT16, x, &setpointCompressed.x)   // position - mm
+LOG_ADD(LOG_INT16, y, &setpointCompressed.y)
+LOG_ADD(LOG_INT16, z, &setpointCompressed.z)
+
+LOG_ADD(LOG_INT16, vx, &setpointCompressed.vx) // velocity - mm / sec
+LOG_ADD(LOG_INT16, vy, &setpointCompressed.vy)
+LOG_ADD(LOG_INT16, vz, &setpointCompressed.vz)
+
+LOG_ADD(LOG_INT16, ax, &setpointCompressed.ax) // acceleration - mm / sec^2
+LOG_ADD(LOG_INT16, ay, &setpointCompressed.ay)
+LOG_ADD(LOG_INT16, az, &setpointCompressed.az)
+LOG_GROUP_STOP(ctrltargetZ)
+
+LOG_GROUP_START(stabilizer)
+LOG_ADD(LOG_FLOAT, roll, &state.attitude.roll)
+LOG_ADD(LOG_FLOAT, pitch, &state.attitude.pitch)
+LOG_ADD(LOG_FLOAT, yaw, &state.attitude.yaw)
+LOG_ADD(LOG_FLOAT, thrust, &control.thrust)
+
+STATS_CNT_RATE_LOG_ADD(rtStab, &stabilizerRate)
+LOG_ADD(LOG_UINT32, intToOut, &inToOutLatency)
+LOG_GROUP_STOP(stabilizer)
+
+LOG_GROUP_START(acc)
+LOG_ADD(LOG_FLOAT, x, &sensorData.acc.x)
+LOG_ADD(LOG_FLOAT, y, &sensorData.acc.y)
+LOG_ADD(LOG_FLOAT, z, &sensorData.acc.z)
+LOG_GROUP_STOP(acc)
+
+#ifdef LOG_SEC_IMU
+LOG_GROUP_START(accSec)
+LOG_ADD(LOG_FLOAT, x, &sensorData.accSec.x)
+LOG_ADD(LOG_FLOAT, y, &sensorData.accSec.y)
+LOG_ADD(LOG_FLOAT, z, &sensorData.accSec.z)
+LOG_GROUP_STOP(accSec)
+#endif
+
+LOG_GROUP_START(baro)
+LOG_ADD(LOG_FLOAT, asl, &sensorData.baro.asl)
+LOG_ADD(LOG_FLOAT, temp, &sensorData.baro.temperature)
+LOG_ADD(LOG_FLOAT, pressure, &sensorData.baro.pressure)
+LOG_GROUP_STOP(baro)
+
+LOG_GROUP_START(gyro)
+LOG_ADD(LOG_FLOAT, x, &sensorData.gyro.x)
+LOG_ADD(LOG_FLOAT, y, &sensorData.gyro.y)
+LOG_ADD(LOG_FLOAT, z, &sensorData.gyro.z)
+LOG_GROUP_STOP(gyro)
+
+#ifdef LOG_SEC_IMU
+LOG_GROUP_START(gyroSec)
+LOG_ADD(LOG_FLOAT, x, &sensorData.gyroSec.x)
+LOG_ADD(LOG_FLOAT, y, &sensorData.gyroSec.y)
+LOG_ADD(LOG_FLOAT, z, &sensorData.gyroSec.z)
+LOG_GROUP_STOP(gyroSec)
+#endif
+
+LOG_GROUP_START(mag)
+LOG_ADD(LOG_FLOAT, x, &sensorData.mag.x)
+LOG_ADD(LOG_FLOAT, y, &sensorData.mag.y)
+LOG_ADD(LOG_FLOAT, z, &sensorData.mag.z)
+LOG_GROUP_STOP(mag)
+
+LOG_GROUP_START(controller)
+LOG_ADD(LOG_INT16, ctr_yaw, &control.yaw)
+LOG_GROUP_STOP(controller)
+
+LOG_GROUP_START(stateEstimate)
+LOG_ADD(LOG_FLOAT, x, &state.position.x)
+LOG_ADD(LOG_FLOAT, y, &state.position.y)
+LOG_ADD(LOG_FLOAT, z, &state.position.z)
+
+LOG_ADD(LOG_FLOAT, vx, &state.velocity.x)
+LOG_ADD(LOG_FLOAT, vy, &state.velocity.y)
+LOG_ADD(LOG_FLOAT, vz, &state.velocity.z)
+
+LOG_ADD(LOG_FLOAT, ax, &state.acc.x)
+LOG_ADD(LOG_FLOAT, ay, &state.acc.y)
+LOG_ADD(LOG_FLOAT, az, &state.acc.z)
+
+LOG_ADD(LOG_FLOAT, roll, &state.attitude.roll)
+LOG_ADD(LOG_FLOAT, pitch, &state.attitude.pitch)
+LOG_ADD(LOG_FLOAT, yaw, &state.attitude.yaw)
+
+LOG_ADD(LOG_FLOAT, qx, &state.attitudeQuaternion.x)
+LOG_ADD(LOG_FLOAT, qy, &state.attitudeQuaternion.y)
+LOG_ADD(LOG_FLOAT, qz, &state.attitudeQuaternion.z)
+LOG_ADD(LOG_FLOAT, qw, &state.attitudeQuaternion.w)
+LOG_GROUP_STOP(stateEstimate)
+
+LOG_GROUP_START(stateEstimateZ)
+LOG_ADD(LOG_INT16, x, &stateCompressed.x)                 // position - mm
+LOG_ADD(LOG_INT16, y, &stateCompressed.y)
+LOG_ADD(LOG_INT16, z, &stateCompressed.z)
+
+LOG_ADD(LOG_INT16, vx, &stateCompressed.vx)               // velocity - mm / sec
+LOG_ADD(LOG_INT16, vy, &stateCompressed.vy)
+LOG_ADD(LOG_INT16, vz, &stateCompressed.vz)
+
+LOG_ADD(LOG_INT16, ax, &stateCompressed.ax)               // acceleration - mm / sec^2
+LOG_ADD(LOG_INT16, ay, &stateCompressed.ay)
+LOG_ADD(LOG_INT16, az, &stateCompressed.az)
+
+LOG_ADD(LOG_UINT32, quat, &stateCompressed.quat)           // compressed quaternion, see quatcompress.h
+
+LOG_ADD(LOG_INT16, rateRoll, &stateCompressed.rateRoll)   // angular velocity - milliradians / sec
+LOG_ADD(LOG_INT16, ratePitch, &stateCompressed.ratePitch)
+LOG_ADD(LOG_INT16, rateYaw, &stateCompressed.rateYaw)
+LOG_GROUP_STOP(stateEstimateZ)
